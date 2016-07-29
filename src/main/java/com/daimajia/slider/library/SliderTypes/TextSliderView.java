4b1b26c6d478f3561ea5328456de1c8f999a1888
feer921 <feer921@163.com>
2016-07-29 17:32:57 +0800

    added/changed/modified：修改TextSliderView，如果Slide的控件的描述没有，则不显示下面的描述栏

--- src/main/java/com/daimajia/slider/library/SliderTypes/TextSliderView.java
+++ src/main/java/com/daimajia/slider/library/SliderTypes/TextSliderView.java
@@ -4,9 +4,11 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import common.base.R;
+import common.base.utils.Util;
 
 
 /**
@@ -21,8 +23,15 @@ public class TextSliderView extends BaseSliderView{
     public View getView() {
         View v = LayoutInflater.from(getContext()).inflate(R.layout.render_type_text,null);
         ImageView target = (ImageView)v.findViewById(R.id.daimajia_slider_image);
-        TextView description = (TextView)v.findViewById(R.id.description);
-        description.setText(getDescription());
+        LinearLayout descLayout = (LinearLayout) v.findViewById(R.id.description_layout);
+        String descText = getDescription();
+        if (Util.isEmpty(descText)) {
+            descLayout.setVisibility(View.GONE);
+        }
+        else{
+            TextView description = (TextView)v.findViewById(R.id.description);
+            description.setText(getDescription());
+        }
         bindEventAndShow(v, target);
         return v;
     }
