fb95418578a39491b839d68a7a26876887bef058
feer921 <feer921@163.com>
2017-06-24 18:51:23 +0800

    欲重写空布局View

---
+++ src/main/java/common/base/views/SuperEmptyLoadingView.java
@@ -0,0 +1,29 @@
+package common.base.views;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * ******************(^_^)***********************<br>
+ * User: 11776610771@qq.com<br>
+ * Date: 2017/6/8<br>
+ * Time: 14:03<br>
+ * <P>DESC:无内容提示布局(空布局)、加载数据中提示布局、其他提示布局
+ * </p>
+ * ******************(^_^)***********************
+ */
+
+public class SuperEmptyLoadingView extends LinearLayout {
+
+    public SuperEmptyLoadingView(Context context) {
+        super(context);
+    }
+
+    public SuperEmptyLoadingView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+}
