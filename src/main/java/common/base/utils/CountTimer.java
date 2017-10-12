0daf7ea735ed2eafd9bb9df782e70c17f4f7b917
lifei <1176610771@qq.com>
2017-10-12 21:25:11 +0800

    添加CountTimer类用于统计一段程序的开始运行到结束运行的耗时时间

---
+++ src/main/java/common/base/utils/CountTimer.java
@@ -0,0 +1,58 @@
+package common.base.utils;
+
+/**
+ * ******************(^_^)***********************<br>
+ * User: 11776610771@qq.com<br>
+ * Date: 2017/10/12<br>
+ * Time: 9:52<br>
+ * <P>DESC:
+ * 统计开始到结束的所用的时间
+ * </p>
+ * ******************(^_^)***********************
+ */
+
+public class CountTimer {
+    private static final String TAG = "CountTimer";
+    private static long startTime;
+
+    public static void start() {
+        startTime = System.currentTimeMillis();
+    }
+
+    public static void end() {
+        end(null);
+    }
+    public static void end(String logTag) {
+        end(logTag, null);
+    }
+
+    public static void end(String logTag, String logExtraInfo) {
+        end(logTag, logExtraInfo, 1);
+    }
+    /**
+     * 结束计时，并且输出打印信息
+     * @param logTag Log 的tag,如果为null 则用本类的TAG="CountTimer"
+     * @param logExtraInfo Log的内容 ： 如果为null,则直接是"-->end() experience time: "，否则，为 "xxxx() -->end() experience time: "
+     * @param logLevel Log输出级别，1：i; 2:w；3：e.
+     */
+    public static void end(String logTag, String logExtraInfo, int logLevel) {
+        long endTiem = System.currentTimeMillis();
+        long wasteTiem = endTiem - startTime;
+        String finalLogTag = logTag == null ? TAG : logTag;
+        String logInfoContent = " -->end() experience time: "+ wasteTiem;
+        if (logExtraInfo != null) {
+            logInfoContent = logExtraInfo + logInfoContent;
+        }
+        switch (logLevel) {
+            case 1:
+                CommonLog.i(finalLogTag, logInfoContent);
+                break;
+            case 2:
+                CommonLog.w(finalLogTag, logInfoContent);
+                break;
+            case 3:
+                CommonLog.e(finalLogTag, logInfoContent);
+                break;
+        }
+    }
+}
