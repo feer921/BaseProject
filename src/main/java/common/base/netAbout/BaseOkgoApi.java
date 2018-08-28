68e506bc635121b2c582d4c9f5a2093e9994e4b7
feer921 <feer921@163.com>
2018-08-28 10:42:33 +0800

    再完善BaseOkgoApi

--- src/main/java/common/base/netAbout/BaseOkgoApi.java
+++ src/main/java/common/base/netAbout/BaseOkgoApi.java
@@ -39,62 +39,17 @@ public class BaseOkgoApi extends BaseApi {
         getWithParams(apiUrl, callback, null, "");
     }
 
-//    /**
-//     * 该接口不太适用本APP内的网络get请求
-//     * @deprecated
-//     * @param wholeUrl
-//     * @param callback
-//     * @param params 当该参数为null时，适用本APP内的网络get请求
-//     */
-//    private static void getWithParams(String wholeUrl, AbsCallback callback, Map<String, String> params) {
-//        GetRequest getRequest = OkGo.get(wholeUrl);
-//        String tsKey = "ts";
-//        String curTimeMills = System.currentTimeMillis() + "";
-//        if (params == null) {
-//            getRequest.params(tsKey, curTimeMills);
-//            String wholeParamStr = tsKey + ":" + curTimeMills;
-//            String singParam = HMACSHA1.genHMAC(wholeParamStr, AppConfigs.TOKEN, true);
-//            getRequest.params("sign", singParam);
-//        }
-//        else{
-//            getRequest.params(params);
-//        }
-//        excute(getRequest, callback);
-//    }
-
-//    /**
-//     *
-//     * @param apiUrl 可以是全路径也可以是半路径,这里自动判断来区分
-//     * @param callback 网络响应的回调
-//     * @param appendKeys get请求的请求参数的keys 如果外部不传，也会有默认的共同参数
-//     * @param values get请求的请求参数的values
-//     */
-//    public static void getWithParams(String apiUrl, AbsCallback callback, String[] appendKeys, String... values) {
-//        String apiHostUrl = apiUrl;
-//        if (!apiUrl.startsWith(URL_HOST)) {
-//            apiHostUrl = URL_HOST + apiUrl;
-//        }
-//        GetRequest getRequest = OkGo.get(apiHostUrl);
-//        buildCommonGetParams(getRequest, appendKeys, values);
-//        excute(getRequest, callback);
-//    }
-
     public static void getWithParams(String wholeApiUrl, AbsCallback callback, String[] appendKeys, String... keyValues) {
         GetRequest getRequest = OkGo.get(wholeApiUrl);
-        buildCommonGetParams(getRequest, appendKeys, keyValues);
+        buildCommonRequestParams(getRequest, appendKeys, keyValues);
         excute(getRequest, callback);
     }
     /**
-     * @param getRequest
+     * @param request
      * @param appendKeys keys
      * @param values values 要一一对应到key的值
      */
-    protected static void buildCommonGetParams(GetRequest getRequest, String[] appendKeys, String... values) {
-//        String commonTsKey = "ts";
-//        String commonCurTimeMills = (System.currentTimeMillis() /1000)+ "";//后台需要秒
-//        getRequest.params(commonTsKey, commonCurTimeMills);
-//        TreeMap<String, String> joinMd5KeyValues = new TreeMap<>();
-//        joinMd5KeyValues.put(commonTsKey, commonCurTimeMills);
+    public static BaseRequest buildCommonRequestParams(BaseRequest request, String[] appendKeys, String... values) {
         if (appendKeys != null && values != null) {//如果有外部传入的参数
             int keysLen = appendKeys.length;
             int valuesLen = values.length;
@@ -102,23 +57,11 @@ public class BaseOkgoApi extends BaseApi {
                 for (int keyIndex = 0; keyIndex < keysLen; keyIndex++) {
                     String curKey = appendKeys[keyIndex];
                     String curValue = values[keyIndex];
-//                    joinMd5KeyValues.put(curKey, curValue);
-                    getRequest.params(curKey, curValue);
+                    request.params(curKey, curValue);
                 }
             }
         }
-//        if (!joinMd5KeyValues.containsKey("uuid")) {
-//            String defUUid = AppConfigs.DEV_UUID;
-//            joinMd5KeyValues.put("uuid",defUUid);
-//            getRequest.params("uuid", defUUid);
-//        }
-//        if (!joinMd5KeyValues.containsKey("expire")) {
-//            if (AppConfigs.TOKEN_EXPIRE_TIME > 0) {
-//                String expireTime = AppConfigs.TOKEN_EXPIRE_TIME + "";
-//                joinMd5KeyValues.put("expire", expireTime);
-//                getRequest.params("expire", expireTime);
-//            }
-//        }
+        return request;
     }
     public static BaseRequest getRequest(String wholeUrl) {
         return createRequest(wholeUrl, GET);
@@ -136,6 +79,18 @@ public class BaseOkgoApi extends BaseApi {
         get(thisWholeUrl,callback);
     }
 
+    /**
+     * 带有请求参数/值，键值对的post请求
+     * @param wholeApiUrl 完整的api url地址
+     * @param callback 回调
+     * @param keysInParams 参数键
+     * @param keysValues 参数值
+     */
+    public static void postWithParams(String wholeApiUrl, AbsCallback callback, String[] keysInParams, String... keysValues) {
+        PostRequest postRequest = postRequest(wholeApiUrl);
+        buildCommonRequestParams(postRequest, keysInParams, keysValues);
+        excute(postRequest, callback);
+    }
     public static void post(String wholeUrl, AbsCallback callback) {
         excute(createRequest(wholeUrl,POST),callback);
     }
@@ -158,7 +113,7 @@ public class BaseOkgoApi extends BaseApi {
         }
         return null;
     }
-    protected static void excute(BaseRequest request, AbsCallback callback) {
+    public static void excute(BaseRequest request, AbsCallback callback) {
         request.tag(request.getUrl()).execute(callback);
     }
 
@@ -256,23 +211,5 @@ public class BaseOkgoApi extends BaseApi {
                 .upJson(jsonBody)
                 .execute(callback);
     }
-//    /**
-//     * 可指定root JSon数据体键、值的 post请求
-//     * 注：主要就是兼容该Post的root(第一级JSon数据体)的键和值外部自定义
-//     *     并且默认为对参数加密
-//     * @param partialUrl 请求地址后缀
-//     * @param callback 响应回调
-//     * @param keysInRoot 最外层JSON 数据的key，除去"param"与"sign"
-//     * @param valuesInRoot 最外层JSON 数据的key对应的值，除去"param"与"sign"两键的值
-//     * @param keysInParam
-//     * @param valuesInParam
-//     */
-//    protected static void jsonPost(String partialUrl, AbsCallback callback, String[] keysInRoot, String[] valuesInRoot, String[] keysInParam, String... valuesInParam) {
-//        JSONObject paramJson = createJsonWithParams(keysInParam, valuesInParam);
-//        JSONObject rootJsonBody = createJsonWithParams(keysInRoot, valuesInRoot);
-//
-//        postRequest(URL_HOST + partialUrl)
-//                .upJson(rootJsonBody)
-//                .execute(callback);
-//    }
+
 }
